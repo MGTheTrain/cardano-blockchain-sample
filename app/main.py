900b715d56aa5306c7f973acb59f1fdb3cbb49d8
unknown <Marvin.Gajek1@web.de>
2023-11-25 14:36:57 +0100

    update README.md

--- app/main.py
+++ app/main.py
@@ -8,3 +8,8 @@ wallet = CardanoWallet(
 # Example: Get network information
 network_info = wallet.get_network_information()
 print(network_info)
+
+# # Example: Create a new wallet
+# wallet_name = "my_wallet"
+# mnemonic = "your space-separated mnemonic words"
+# wallet.create_wallet(wallet_name, mnemonic)
